e008993990a2353ca4aae1b4014a860ebcd3cb94
PasitU <Pasit.udom@mail.kmutt.ac.th>
2023-03-20 19:13:31 +0700

    modified file

--- MyProject.java
+++ MyProject.java
@@ -3,5 +3,6 @@ package myproject;
 public class MyProject {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello world!");
     }
 }
